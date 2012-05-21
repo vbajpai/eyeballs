c9e875ff8beee709f80507b6ca20164c9093d39c
Vaibhav Bajpai <contact@vaibhavbajpai.com>
2012-05-21 22:23:01 +0200

    * using ns_parseerr(...) to parse a resource record section * using ns_rr_X(...) to get specific fields of resource record * using ns_sprintrr(...) to pretty print a resource record (DEBUG) * using res_pquery(...) to pretty print DNS response (DEBUG) * using do_section(...) to pretty print DNS answers section (DEBUG)

--- src/dns.c
+++ src/dns.c
@@ -34,6 +34,80 @@
 #include <arpa/nameser.h>
 #include <resolv.h>
 #include <netdb.h>
+#include <errno.h>
+#include <string.h>
+
+static void
+do_section(const res_state statp,
+           ns_msg *handle, 
+           ns_sect section,
+           FILE *file) {
+
+  int n, rrnum;
+  static int buflen = 2048;
+  char *buf;
+  ns_opcode opcode;
+  ns_rr rr;
+
+  buf = malloc(buflen);
+  if (buf == NULL) {
+    fprintf(file, ";; memory allocation failure\n");
+    return;
+  }
+
+  opcode = (ns_opcode) ns_msg_getflag(*handle, ns_f_opcode);
+  rrnum = 0;
+  for (;;) {
+    if (ns_parserr(handle, section, rrnum, &rr)) {
+      if (errno != ENODEV)
+        fprintf(file, ";; ns_parserr: %s\n",
+          strerror(errno));
+      else if (rrnum > 0)
+        putc('\n', file);
+      goto cleanup;
+    }
+    if (rrnum == 0)
+      fprintf(file, ";; %s SECTION:\n",
+        p_section(section, opcode));
+    if (section == ns_s_qd)
+      fprintf(file, ";;\t%s, type = %s, class = %s\n",
+        ns_rr_name(rr),
+        p_type(ns_rr_type(rr)),
+        p_class(ns_rr_class(rr)));
+    else if (section == ns_s_ar && ns_rr_type(rr) == ns_t_opt) {
+      u_int32_t ttl = ns_rr_ttl(rr);
+      fprintf(file,
+        "; EDNS: version: %u, udp=%u, flags=%04x\n",
+        (ttl>>16)&0xff, ns_rr_class(rr), ttl&0xffff);
+    } else {
+      n = ns_sprintrr(handle, &rr, NULL, NULL,
+          buf, buflen);
+      if (n < 0) {
+        if (errno == ENOSPC) {
+          free(buf);
+          buf = NULL;
+          if (buflen < 131072)
+            buf = malloc(buflen += 1024);
+          if (buf == NULL) {
+            fprintf(file,
+                      ";; memory allocation failure\n");
+                return;
+          }
+          continue;
+        }
+        fprintf(file, ";; ns_sprintrr: %s\n",
+          strerror(errno));
+        goto cleanup;
+      }
+      fputs(buf, file);
+      fputc('\n', file);
+    }
+    rrnum++;
+  }
+ cleanup:
+  if (buf != NULL)
+    free(buf);
+} 
 
 int main(int argc, char* argv[]) {
 
@@ -74,6 +148,10 @@ int main(int argc, char* argv[]) {
                   ) < 0
      )
     herror("ns_initparse(...)");
+#ifdef DEBUG
+  res_pquery(&_res, answer, answerlen, stdout);
+#endif
+  do_section(&_res, &handle, ns_s_an, stdout);
 
   /* get msg_id */
   u_int16_t msg_id = ns_msg_id(handle);
@@ -84,11 +162,46 @@ int main(int argc, char* argv[]) {
   u_int16_t an_count = ns_msg_count(handle, ns_s_an);
   u_int16_t ns_count = ns_msg_count(handle, ns_s_ns);
   u_int16_t ar_count = ns_msg_count(handle, ns_s_ar);
-
   printf("\nqd_count: %u", qd_count);
   printf("\nan_count: %u", an_count);
   printf("\nns_count: %u", ns_count);
   printf("\nar_count: %u", ar_count);
+  
+  /* parse a resource record section */
+  ns_rr rr;
+  if (
+      ns_parserr(
+                 &handle,    /* data structure filled by ns_initparse */
+                 ns_s_an,    /* resource record section */
+                 1,          /* index of the resource record in this section */   
+                 &rr         /* data structure filled in by ns_parseerr */
+                ) < 0
+     )   
+    herror("ns_parserr(...)");
+#ifdef DEBUG
+  printf("\nns_rr_name: %s", ns_rr_name(rr));
+  printf("\nns_rr_type: %u", ns_rr_type(rr));
+  printf("\nns_rr_class: %u", ns_rr_class(rr));
+  printf("\nns_rr_ttl: %u", ns_rr_ttl(rr));
+  printf("\nns_rr_rdlen: %u", ns_rr_rdlen(rr));
+  printf("\nns_rr_rdata: %s", ns_rr_rdata(rr));
+#endif
 
+  /* get RR in a presentation format */
+  char buf[100];
+  if (
+      ns_sprintrr(
+                  &handle,
+                  &rr,
+                  NULL,
+                  NULL,
+                  (char*) &buf,
+                  sizeof(buf)
+                 ) < 0
+     )
+    herror("ns_sprintrr(...)");  
+#ifdef DEBUG
+  printf("\nbuffer: %s", buf);
+#endif
   return(EXIT_SUCCESS);
 }
